038bff79037ff5a1c345cc91a98c46cc39fad40a
MisbahNayab12 <160389499+MisbahNayab12@users.noreply.github.com>
2024-10-17 22:40:14 +0500

    Created Next.js Components

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import About from './component/About'
+import Header from './component/Header'
+
+export default function page(){
+  return (
+    <div>
+      <About/>
+      <Header/>
+    </div>
+  )
+}
+
+
